e27ee3bc6b7973a864ccfa3072492e36b2a9fbe0
jianguo <fanjianguo@ulaiber.com>
2015-08-13 16:24:49 +0800

    add hello

---
+++ GitTest/GitTest/hello.h
@@ -0,0 +1,13 @@
+//
+//  hello.h
+//  GitTest
+//
+//  Created by 樊建国 on 15/8/13.
+//  Copyright (c) 2015年 ulaiber. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface hello : NSObject
+
+@end
---
+++ GitTest/GitTest/hello.m
@@ -0,0 +1,13 @@
+//
+//  hello.m
+//  GitTest
+//
+//  Created by 樊建国 on 15/8/13.
+//  Copyright (c) 2015年 ulaiber. All rights reserved.
+//
+
+#import "hello.h"
+
+@implementation hello
+
+@end
