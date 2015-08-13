76034272f915242eb19905f42c593f24b9cd241b
jianguo <fanjianguo@ulaiber.com>
2015-08-13 16:36:24 +0800

    addtool

---
+++ GitTest/tool.h
@@ -0,0 +1,13 @@
+//
+//  tool.h
+//  GitTest
+//
+//  Created by 樊建国 on 15/8/13.
+//  Copyright (c) 2015年 ulaiber. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface tool : NSObject
+
+@end
---
+++ GitTest/tool.m
@@ -0,0 +1,13 @@
+//
+//  tool.m
+//  GitTest
+//
+//  Created by 樊建国 on 15/8/13.
+//  Copyright (c) 2015年 ulaiber. All rights reserved.
+//
+
+#import "tool.h"
+
+@implementation tool
+
+@end
