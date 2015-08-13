3c7529201f3502c0751822466d3e471a69873b9c
jianguo <fanjianguo@ulaiber.com>
2015-08-13 15:44:52 +0800

    nsinter i

--- GitTest/GitTest/AppDelegate.m
+++ GitTest/GitTest/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    NSInteger i = 0;
     NSLog(@"hello world");
     return YES;
 }
